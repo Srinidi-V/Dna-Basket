9bbf871243bb44b041bed11ca823410c7b383885
Srinidi-V <srinidiveer11@gmail.com>
2020-11-19 21:30:19 +0530

    main.py : Add statements to invoke functions to perfom dna fragment synthesis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from inputModification import *
 from stringGeneration import *
+from dnaFragmentSynthesis import *
 
 items={}
 Ds=[]
@@ -9,8 +10,18 @@ connection_code={}
 items=Input_Modification()
 Ds,Dx=GenerateStrands(items)
 connection_code=ConnectionCode()
+"""
 with open("strings.csv",'w') as tf:
     csvwriter=csv.writer(tf)
     csvwriter.writerow(['S.No','Weight','Value'])
     for i in range(len(Ds)):
-        csvwriter.writerow([i+1,Ds[i],Dx[i]])
+        csvwriter.writerow([i+1,Ds[i],Dx[i]])"""
+items["Ds"] = Ds
+items["Dx"] = Dx
+items["connection_code"] = connection_code
+
+
+selected = SynthesizeDnaFragments(items)
+items ["selected_fragments"] = selected["selected_fragments"]
+items ["selected_items"] = selected["selected_items"]
+print(items)
