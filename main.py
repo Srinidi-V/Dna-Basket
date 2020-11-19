da0ddcf95c7f9feeefe6f31b110c2ede706e8348
Sangeetha V D <sangeetha2000.vd@gmail.com>
2020-11-20 00:45:16 +0530

    main.py: A call is made to maxProfitItems()

--- main.py
+++ main.py
@@ -1,27 +1,26 @@
 from inputModification import *
 from stringGeneration import *
 from dnaFragmentSynthesis import *
+from maxValue import *
 
 items={}
-Ds=[]
-Dx=[]
-connection_code={}
+result={}
 
-items=Input_Modification()
-Ds,Dx=GenerateStrands(items)
-connection_code=ConnectionCode()
-"""
-with open("strings.csv",'w') as tf:
+items,mul_factor=Input_Modification()
+items["Ds"],items["Dx"]=GenerateStrands(items)
+items["connection_code"]=ConnectionCode()
+
+"""with open("strings.csv",'w') as tf:
     csvwriter=csv.writer(tf)
     csvwriter.writerow(['S.No','Weight','Value'])
     for i in range(len(Ds)):
         csvwriter.writerow([i+1,Ds[i],Dx[i]])"""
-items["Ds"] = Ds
-items["Dx"] = Dx
-items["connection_code"] = connection_code
-
-
+        
 selected = SynthesizeDnaFragments(items)
 items ["selected_fragments"] = selected["selected_fragments"]
 items ["selected_items"] = selected["selected_items"]
-print(items)
+
+result=maxProfitItems(items)
+print("Items included: " + str(final_items["items"]))
+print("Maximum Profit: " + str(final_items["highest_profit"]))
+print("Total Weight: " + str(final_items["highest_weight"]//mul_factor))
